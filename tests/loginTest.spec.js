8ed7e9a5d8300d88443c372c7efd718c525360c7
Felipe Slobodzian <felipeslobodzian3@gmail.com>
2025-06-24 10:28:17 -0300

    first commit

---
+++ tests/loginTest.spec.js
@@ -0,0 +1,26 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('Teste de Login Válido', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/login');
+  await page.locator('#user').fill('felipeslob@gmail.com');
+  await page.locator('#password').fill('123456');
+  await page.getByRole('button', { name: 'login' }).click();
+  await expect(page.getByRole('heading', { name: 'Login realizado' })).toBeVisible();
+});
+
+test('Teste de Login Inválido - E-mail Vazio', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/login');
+  await page.locator('#user').fill('');
+  await page.locator('#password').fill('123456');
+  await page.getByRole('button', { name: 'login' }).click();
+  await expect(page.getByText('E-mail inválido.')).toBeVisible();
+});
+
+test('Teste de Login Inválido - Senha Vazia', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/login');
+  await page.locator('#user').fill('felipeslob@gmail.com');
+  await page.locator('#password').fill('');
+  await page.getByRole('button', { name: 'login' }).click();
+  await expect(page.getByText('Senha inválida.')).toBeVisible();
+});
